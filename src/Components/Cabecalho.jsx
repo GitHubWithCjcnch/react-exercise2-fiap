328c0d2f292b77a7aab013235863a2f88703527d
Emii2003 <emile.maia@etec.sp.gov.br>
2023-09-14 22:03:59 -0300

    Estilizando o header

--- src/Components/Cabecalho.jsx
+++ src/Components/Cabecalho.jsx
@@ -1,11 +1,11 @@
 import { Link } from "react-router-dom";
-
+import cabecalho from "../css/Cabecalho.css";
 export default function Cabecalho(){
 
     return(
         <>
             <header>
-                <h1>Projeto da inserção </h1>
+                <h1>Cadastro de produtos</h1>
                 <ul>
                     <li><Link to="/">Home</Link></li>
                     <li><Link to="/produtos">Produtos</Link></li>
