1cca1b9420395521c3d470ad6f23ab5a558785b6
Str1nder <rm552238@fiap.com.br>
2023-09-14 23:07:41 -0300

    Estilizando rodapé

--- src/Components/Rodape.jsx
+++ src/Components/Rodape.jsx
@@ -1,8 +1,14 @@
+import styles from "../css/Rodape.module.css"
+
 export default function Rodape(){
      return (
         <>
-            <footer>
-                <p>&copy; Todos os meus direitos reservados. 2023</p>
+            <footer className={styles.footer}>
+                <div className={styles.footerDiv}>
+                    <h2 className={styles.footerDivh2}>Entre em Contato</h2>
+                    <p className={styles.footerDivp}>Email: thinkingfive10@gmail.com</p>
+                </div>
+                <p className={styles.footerText}>&copy; Todos os meus direitos reservados. 2023</p>
             </footer>
         </>
      )
