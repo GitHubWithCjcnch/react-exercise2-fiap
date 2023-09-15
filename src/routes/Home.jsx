f45e4c231412c02d0efaae46f6e0386960919dc3
julia.andrade-at-538590928221 <qoTQt0Cryp6/ysKmwHlONj6KyLQNdpMjVwl0qeqlNns=>
2023-09-15 00:41:40 -0300

    add meme - home

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,6 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+import meme from "../assets/meme.jpeg";
+import '../css/Home.css';
 
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="center-image">
+      <img src={meme} alt="Meme sz" />
+    </div>
   )
 }
